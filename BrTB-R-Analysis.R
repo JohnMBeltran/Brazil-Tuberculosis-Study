45f6bcf4f5a73b68fcdb7fcc954fe1acea929847
John <John.MBeltran@outlook.com>
2021-04-01 12:30:57 +0100

    A

--- BrTB-R-Analysis.R
+++ BrTB-R-Analysis.R
@@ -1,2 +1,17 @@
 require(tidyverse)
+require(sp)
+require(mgcv)
+require(maps)
+require(fields)
 load("TB.RData")
+head(TBdata)
+
+
+plot.map(TBdata$TB[TBdata$Year==2014],n.levels=7,main="TB counts for 2014")
+
+
+
+
+
+
+
